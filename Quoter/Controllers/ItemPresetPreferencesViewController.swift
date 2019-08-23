0b3174a968cb2a35fd74ad4992d925a84cb180df
b-low <bclow@umich.edu>
2019-08-23 12:40:04 -1000

    Mockups for other interfaces

---
+++ Quoter/Controllers/ItemPresetPreferencesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ItemPresetPreferencesViewController.swift
+//  Quoter
+//
+//  Created by Brandon Low on 8/23/19.
+//  Copyright © 2019 Brandon Low. All rights reserved.
+//
+
+import Cocoa
+
+class ItemPresetPreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    // TODO: Generate table views programatically from entity properties for columns, rename file
+    
+}
